4402caa9487854e456e9c174f25f2a048975b61f
Ethan Blahay <eblahay@protonmail.com>
2022-10-04 10:36:56 -0400

    ver. 0.1.0

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
 # main application file
 
+from argparse import ArgumentParser
+
+
 # return true if x is an even integer
 def isEven(x):
-    # stores the remainder of "x / 2"
     return (x % 2 == 0)
 
 
 # main function
 def main():
-    b = isEven(5)
-    
-    if(b == True):
+    parser = ArgumentParser()
+
+    parser.add_argument('x', type=int, help="an integer which will have its parity determined")
+    parser.add_argument(
+        '--version', 
+        action='version', 
+        version='odd-or-even 0.1.0', 
+        help="prints out this program's version information and then quits"
+    )
+
+    args = parser.parse_args()
+
+    # determine the parity of the int & output the result
+    if(isEven(args.x)):
         print("even")
     else:
         print("odd")
